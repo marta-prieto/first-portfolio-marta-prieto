125e2aff8acb72ad8f448c62548d2ba56639c09a
Jorge <martaprietoluque@gmail.com>
2019-10-12 21:22:17 +0200

    Add first structure

--- src/App.js
+++ src/App.js
@@ -2,25 +2,36 @@ import React from 'react';
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-       
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <header className="app__header">
+          <h1 className="app__title">Marta Prieto</h1>
+          <img className="app__image"></img>
+          <h2 className="app__about-me">Sobre mí</h2>
+          <p></p>
+
+          <h2 className="app__projects">Proyectos</h2>
+          <a></a>
+          <a></a>
+          <a></a>
+          <a></a>
+
+          <h2 className="app__contact-links">Contacto</h2>
+          <a></a>
+          <a></a>
+          <a></a>
+          <a></a>
+
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
