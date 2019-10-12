c10f4ca5a84491aa3ad7ad7b6f539db1fe94e394
Jorge <martaprietoluque@gmail.com>
2019-10-13 00:03:11 +0200

    Add scss main menu

--- src/App.js
+++ src/App.js
@@ -12,22 +12,23 @@ class App extends React.Component {
       <div className="app">
         <header className="app__header">
           <h1 className="app__title">Marta Prieto</h1>
-          <img className="app__image"></img>
-          <h2 className="app__about-me">Sobre mí</h2>
-          <p></p>
+          <img className="app__image" src="../src/image/foto-marta.jpg" alt="foto Marta"></img>
+          <div className="menu__container">
+            <h2 className="app__about-me">Mi historia</h2>
+            <p></p>
 
-          <h2 className="app__projects">Proyectos</h2>
-          <a></a>
-          <a></a>
-          <a></a>
-          <a></a>
+            <h2 className="app__projects">Proyectos</h2>
+            <a></a>
+            <a></a>
+            <a></a>
+            <a></a>
 
-          <h2 className="app__contact-links">Contacto</h2>
-          <a></a>
-          <a></a>
-          <a></a>
-          <a></a>
-
+            <h2 className="app__contact-links">Contacto</h2>
+            <a></a>
+            <a></a>
+            <a></a>
+            <a></a>
+          </div>
         </header>
       </div>
     );
